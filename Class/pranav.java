6705bd61068fd87b3a80bc0ad05eea5231029e85
pranavmehta95 <145219665+pranavmehta95@users.noreply.github.com>
2025-06-04 16:02:20 +0530

    unusual

--- Class/pranav.java
+++ Class/pranav.java
@@ -1,8 +1,15 @@
-package Class;
+// package Class;
 
-public class pranav {
-    public static void main(String[] args) {
-        System.out.println("pranav");
-    }
+// public class pranav {
+//     public static void main(String[] args) {
+//         System.out.println("pranav");
+//     }
     
+// }
+
+package Class;
+public class pranav{
+    public static void main(String[] args) {
+        System.out.println("pranav kumar");
+    }
 }
