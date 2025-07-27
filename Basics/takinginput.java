0bd113fbffb1903225613492069a1af57eb503c7
pranavmehta95 <145219665+pranavmehta95@users.noreply.github.com>
2025-07-27 19:23:20 +0530

    initial commit

---
+++ Basics/takinginput.java
@@ -0,0 +1,11 @@
+package Basics;
+import java.util.Scanner;
+public class takinginput {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println(n*n);
+
+    }
+}
