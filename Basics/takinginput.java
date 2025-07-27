87d34894ca0099783b9b2623f625415e040e3c9d
pranavmehta95 <145219665+pranavmehta95@users.noreply.github.com>
2025-07-27 22:53:01 +0530

    initial commit

--- Basics/takinginput.java
+++ Basics/takinginput.java
@@ -1,3 +1,16 @@
+// package Basics;
+// import java.util.Scanner;
+// public class takinginput {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter a number: ");
+//         int n = sc.nextInt();
+//         System.out.println(n*n);
+
+//     }
+// }
+
+
 package Basics;
 import java.util.Scanner;
 public class takinginput {
@@ -5,7 +18,10 @@ public class takinginput {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
-        System.out.println(n*n);
+        System.out.print("Enter 2nd number : ");
+        int m = sc.nextInt();
+        int j = n + m;
+        System.out.println("Sum is : "+j);
 
     }
 }
