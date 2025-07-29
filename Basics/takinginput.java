a144e1097e68844b9cccb6dbfd8c1c1662fda37c
pranavmehta95 <145219665+pranavmehta95@users.noreply.github.com>
2025-07-29 22:26:34 +0530

    initial commit

--- Basics/takinginput.java
+++ Basics/takinginput.java
@@ -1,16 +1,3 @@
-// package Basics;
-// import java.util.Scanner;
-// public class takinginput {
-//     public static void main(String[] args) {
-//         Scanner sc=new Scanner(System.in);
-//         System.out.print("Enter a number: ");
-//         int n = sc.nextInt();
-//         System.out.println(n*n);
-
-//     }
-// }
-
-
 package Basics;
 import java.util.Scanner;
 public class takinginput {
@@ -18,10 +5,23 @@ public class takinginput {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
-        System.out.print("Enter 2nd number : ");
-        int m = sc.nextInt();
-        int j = n + m;
-        System.out.println("Sum is : "+j);
+        System.out.println(n*n);
 
     }
 }
+
+
+// package Basics;
+// import java.util.Scanner;
+// public class takinginput {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter a number: ");
+//         int n = sc.nextInt();
+//         System.out.print("Enter 2nd number : ");
+//         int m = sc.nextInt();
+//         int j = n + m;
+//         System.out.println("Sum is : "+j);
+
+//     }
+// }
---
+++ Conditional/ifelase.java
@@ -0,0 +1,15 @@
+package Conditional;
+
+public class ifelase {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        if(a>b){
+            System.out.print("Yes it's true.");
+        }
+        else{
+            System.out.print("nope.");
+        }
+        
+    }
+}
