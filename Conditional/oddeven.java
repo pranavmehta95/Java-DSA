05418c3d835c062cc6caf1cb7c679a787a401293
pranavmehta95 <145219665+pranavmehta95@users.noreply.github.com>
2025-07-31 23:10:37 +0530

    initial commit

--- Conditional/oddeven.java
+++ Conditional/oddeven.java
@@ -1,13 +1,31 @@
+// package Conditional;
+// import java.util.Scanner;
+// public class oddeven {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a number : ");
+//         int n = sc.nextInt();
+//         if(n%2 == 0) System.out.println("Even");
+//         if(n%2 != 0) System.out.println("Odd");
+
+//     }
+    
+// }
+
+
+
 package Conditional;
 import java.util.Scanner;
 public class oddeven {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number : ");
-        int n = sc.nextInt();
-        if(n%2 == 0) System.out.println("Even");
-        if(n%2 != 0) System.out.println("Odd");
-
-    }
+        int time = 22;
+        if (time < 10) {
+          System.out.println("Good morning.");
+        } else if (time < 18) {
+          System.out.println("Good day.");
+        }  else {
+          System.out.println("Good evening.");
+        }
+      }
     
 }
