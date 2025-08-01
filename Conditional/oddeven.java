3257ef6d6db15952ead078646270ad98c5396e78
pranavmehta95 <145219665+pranavmehta95@users.noreply.github.com>
2025-08-01 23:48:15 +0530

    initial commit

--- Conditional/oddeven.java
+++ Conditional/oddeven.java
@@ -17,15 +17,31 @@
 package Conditional;
 import java.util.Scanner;
 public class oddeven {
-    public static void main(String[] args) {
-        int time = 22;
-        if (time < 10) {
-          System.out.println("Good morning.");
-        } else if (time < 18) {
-          System.out.println("Good day.");
-        }  else {
-          System.out.println("Good evening.");
-        }
-      }
+  public static void main(String[] args) {
+    int day = 4;
+    switch (day) {
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;
+    }
+  }
     
 }
