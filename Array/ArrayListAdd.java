00d3c09e8d0d380ba1c8af1cbd431380cbddbbaa
Pranav Kumar <pranavmehta192@gmail.com>
2025-10-12 21:27:07 +0530

    initial commit

--- Array/ArrayListAdd.java
+++ Array/ArrayListAdd.java
@@ -1,14 +1,23 @@
+// package Array;
+// import java.util.ArrayList;
+// public class ArrayListAdd {
+//     public static void main(String[] args) {
+//         ArrayList<Integer> list = new ArrayList<>();
+//         System.out.println(list);
+//         list.add(0,10);
+//         System.out.println(list);
+        
+
+//    }
+
+// }
+        
+
 package Array;
 import java.util.ArrayList;
 public class ArrayListAdd {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        System.out.println(list);
-        list.add(0,10);
-        System.out.println(list);
-        
 
    }
 
 }
-        
