9d88bb897948e545cbb4429d3e29ef3bcf3c4e21
Pranav Kumar <pranavmehta192@gmail.com>
2025-10-16 19:26:23 +0530

    initial commit

--- Array/sumofelement.java
+++ Array/sumofelement.java
@@ -21,23 +21,47 @@
 
 
 
+// package Array;
+// import java.util.Scanner;
+// public class sumofelement {
+//     public static void main(String[] args) {
+//         Scanner sc  = new Scanner(System.in);
+//         System.out.println("Enter the size of array: ");
+//         int size = sc.nextInt();
+//         int arr[] = new int[size];
+//         System.out.println("Enter the elements of array: ");
+//         for(int i=0;i<arr.length;i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         int product=1;
+//         for(int i=0;i<arr.length;i++){
+//             product=product*arr[i];
+//         }
+//         System.out.println("The product of elements in array is: "+product);
+//     }
+    
+// }
+
+
+
 package Array;
 import java.util.Scanner;
 public class sumofelement {
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in);
-        System.out.println("Enter the size of array: ");
         int size = sc.nextInt();
         int arr[] = new int[size];
-        System.out.println("Enter the elements of array: ");
         for(int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
         }
-        int product=1;
+        int target = sc.nextInt();
         for(int i=0;i<arr.length;i++){
-            product=product*arr[i];
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
         }
-        System.out.println("The product of elements in array is: "+product);
     }
     
 }
---
+++ Array/twoSum.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class twoSum {
+    public static void main(String[] args) {
+        int[] arr = {3,-1,8,5,4,9,2};
+        int x = 9;
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]+arr[j]==x){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+    }
+    
+}
