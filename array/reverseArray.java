57605fe15caf9e434dd1de8e125f80f2adcc32a9
Pranav Kumar <pranavmehta192@gmail.com>
2025-10-16 23:29:33 +0530

    initial commit

--- array/reverseArray.java
+++ array/reverseArray.java
@@ -1,44 +1,73 @@
+// package array;
+// import java.util.Scanner;
+// public class reverseArray {
+//     public static void main(String[] args) {
+//         int arr[] = {23,43,54,22,45,46,34,70,64};
+//         int n = arr.length;
+//         for(int ele: arr){
+//             System.out.print(ele+" ");
+//         }
+//         System.out.println();
+//         //reversing the array
+//         // for(int i=0;i<n/2;i++){
+//         //     int j=n-1-i;
+//         //     int temp = arr[i];
+//         //     arr[i] = arr[j];
+//         //     arr[j] = temp;
+//         // }
+
+//         //or [With the two pointer approach]
+//         // int i = 1, j = 5;   - Part of array reverse kaise kree that's how it works.
+//         int i = 0, j = n-1;
+//         while(i<=j){
+//             // int temp = arr[i];
+//             // arr[i] = arr[j];
+//             // arr[j] =temp;
+
+//             //or i can use funtion to swap
+//             swap(arr,i,j);
+//             i++;
+//             j--;
+//         }
+
+//         for(int ele: arr){
+//             System.out.print(ele+" ");
+//         }
+//         System.out.println();
+//     }
+//     public static void swap(int[] arr, int i, int j) {
+//         int temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//     }
+    
+// }
+
+
+
+
 package array;
 import java.util.Scanner;
 public class reverseArray {
     public static void main(String[] args) {
-        int arr[] = {23,43,54,22,45,46,34,70,64};
-        int n = arr.length;
-        for(int ele: arr){
-            System.out.print(ele+" ");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for(int i=0;i<size;i++){
+            arr[i] = sc.nextInt();
         }
-        System.out.println();
-        //reversing the array
-        // for(int i=0;i<n/2;i++){
-        //     int j=n-1-i;
-        //     int temp = arr[i];
-        //     arr[i] = arr[j];
-        //     arr[j] = temp;
-        // }
-
-        //or [With the two pointer approach]
-        // int i = 1, j = 5;   - Part of array reverse kaise kree that's how it works.
-        int i = 0, j = n-1;
+        int n = arr.length;
+        int i = 0, j = size-1;
         while(i<=j){
-            // int temp = arr[i];
-            // arr[i] = arr[j];
-            // arr[j] =temp;
-
-            //or i can use funtion to swap
-            swap(arr,i,j);
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
             i++;
             j--;
         }
-
         for(int ele: arr){
-            System.out.print(ele+" ");
-        }
+             System.out.print(ele+" ");
+         }
         System.out.println();
     }
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    
 }
