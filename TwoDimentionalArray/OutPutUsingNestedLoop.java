2eb2bd4bc2909ccbfd32ae357fe327a41a85e822
Pranav Kumar <pranavmehta192@gmail.com>
2025-10-19 20:57:32 +0530

    initial commit

---
+++ TwoDimentionalArray/OutPutUsingNestedLoop.java
@@ -0,0 +1,24 @@
+package TwoDimentionalArray;
+import java.util.Scanner;
+public class OutPutUsingNestedLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] grid = new int[2][3];
+        int m = grid.length;        // number of rows
+        System.out.println(m);  //number of rows
+        int n = grid[0].length;     // number of columns
+        System.out.println(n);  //number of columns
+        // for(int i=0;i<2;i++){
+        //     for(int j=0;j<3;j++){
+        //         grid[i][j] = sc.nextInt();
+        //     }
+        // }
+
+        // for(int i=0;i<2;i++){
+        //     for(int j=0;j<3;j++){
+        //         System.out.print(grid[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
