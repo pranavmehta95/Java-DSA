a11c291ded5b7eabf76d766b2b5bc1cbb3eb9efc
Pranav Kumar <pranavmehta192@gmail.com>
2025-10-27 18:55:49 +0530

    initial commit

---
+++ array/aaaLeetCode.java
@@ -0,0 +1,46 @@
+// Two Sum Problem
+// package array;
+// import java.util.Scanner;
+// public class aaaLeetCode {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int[] arr = new int[5];
+//         int[] ans = new int[2];
+//         int x = 10;
+//         int n = arr.length;
+//         System.out.println("Enter the elements of the array: ");
+//         for(int i=0;i<n;i++){
+//             arr[i] = sc.nextInt();
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             boolean found = false;
+//             for(int j=i+1;j<n;j++){
+//                 if(arr[i]+arr[j]==x){
+//                     ans[0] = i;
+//                     ans[1] = j;
+//                     found = true;
+//                     break;
+//                 }
+//             }
+//             if(found==true){
+//                 break;
+//             }
+//         }
+//         System.out.println("Indices of the two numbers such that they add up to "+x+" are: "+ans[0]+" and "+ans[1]);
+//     }
+// }
+
+
+
+
+
+
+
+package array;
+import java.util.Scanner;
+public class aaaLeetCode {
+    public static void main(String[] args) {
+        
+    }
+}
