ae96f88415e352919e859130e359ed234dd1bd96
Pranav Kumar <pranavmehta192@gmail.com>
2025-11-03 18:34:57 +0530

    initial commit

---
+++ TwoDimentionalArray/addtwogivenmatrics.java
@@ -0,0 +1,37 @@
+package TwoDimentionalArray;
+
+import java.util.Scanner;
+
+public class addtwogivenmatrics {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int arr1[][] = new int[m][n];
+        int arr2[][] = new int[m][n];
+        int arr3[][] = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr3[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(arr3[i][j]+" ");
+            }
+        }
+
+
+    }
+    
+}
---
+++ TwoDimentionalArray/rowswisecolumnwise.java
@@ -0,0 +1,51 @@
+// package TwoDimentionalArray;
+// // import java.util.Scanner;
+// public class rowswisecolumnwise {
+//     public static void main(String[] args) {
+//         int[][] arr = {{12,32,32,34},{26,87,64,36},{10,20,30,40}};
+//         int m = arr.length, n=arr[0].length;
+//         // for (int i = 0; i < m; i++) {
+//         //     for(int j=0;j<n;j++){
+//         //         System.out.print(arr[i][j]+" ");
+//         //     }
+//         //     System.out.println();
+//         // }
+
+//         //columnwise printing
+//         for (int j = 0; j < n; j++) {
+//             for(int i=0;i<m;i++){
+//                 System.out.print(arr[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+    
+// }
+
+
+
+package TwoDimentionalArray;
+// import java.util.Scanner;
+public class rowswisecolumnwise {
+    public static void main(String[] args) {
+        int[][] arr = {{12,32,32,34},{26,87,64,36},{10,20,30,40}};
+        int m = arr.length, n = arr[0].length;
+        for (int i = 0; i < m; i++) {
+            for(int j = 0;j<n ;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        int[][] transpose = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++){
+                transpose[i][j] = arr[j][i];
+                System.out.print(transpose[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
